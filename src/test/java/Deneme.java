d32610da49dc7ac59a9b8fb51eeac2df98ed6deb
Sezen <aysezen6@gmail.com>
2023-12-18 23:14:17 +0100

    V2

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,6 +1,10 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Ben burdayim!");
+        System.out.println("Degisiklik yaptim!!");
+        System.out.println("Bir sey daha ekledim");
+        System.out.println("Iki sey daha ekledim");
+        System.out.println("dort sey daha ekledim");
     }
 }
 
